115b1083b3ae44ad76c9eccc0e9b1d85ce9f4868
Zhaox1aofu <841432551@qq.com>
2021-04-03 21:47:37 +0800

    '首页开发'

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,13 +1,21 @@
 <template>
   <h2>我的</h2>
+
 </template>
 
 <script>
+import Scroll from "@/components/common/scroll/Scroll";
+
 export default {
-  name: "Profile"
+  name: "Profile",
+  components: {
+    Scroll
+  }
 }
 </script>
 
 <style scoped>
+  .content {
 
+  }
 </style>
